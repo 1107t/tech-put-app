885fc3301659af13952d09db9f0518e5876c169a
yuji426 <king_7642@yahoo.co.jp>
2026-01-18 21:27:01 +0900

    refactor: extract icons

---
+++ src/components/Icons.tsx
@@ -0,0 +1,40 @@
+// src/components/Icons.tsx
+import type React from "react";
+
+type IconProps = React.SVGProps<SVGSVGElement> & {
+  size?: number;
+};
+
+export function MailIcon({ size = 18, ...props }: IconProps) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width={size}
+      height={size}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      {...props}
+    >
+      <path d="M4 6h16v12H4z" />
+      <path d="M4 8l8 5 8-5" />
+    </svg>
+  );
+}
+
+export function LockIcon({ size = 18, ...props }: IconProps) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      width={size}
+      height={size}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      {...props}
+    >
+      <path d="M7 11V8a5 5 0 0 1 10 0v3" />
+      <path d="M6 11h12v10H6z" />
+    </svg>
+  );
+}
