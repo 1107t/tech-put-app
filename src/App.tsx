ff994f5400de457bbc18a2c5a5a8cb59bca254d4
yuji426 <king_7642@yahoo.co.jp>
2026-01-28 16:31:13 +0900

    chore: sync App routes from main

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,21 @@ import LoginPage from "./pages/users/LoginPage";
 import SignUpPage from "./pages/users/SignUpPage";
 import Passreset from "./pages/users/Passreset";
 import MessagePage from "./pages/users/MessagePage";
+import DashboardPage from "./pages/users/DashboardPage";
 
 export default function App() {
   return (
     <Routes>
-      {/* 最初に開いたら login へ */}
       <Route path="/" element={<Navigate to="/login" replace />} />
 
-      {/* users */}
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignUpPage />} />
       <Route path="/reset" element={<Passreset />} />
 
-      {/* メッセージ */}
+      <Route path="/dashboard" element={<DashboardPage />} />
+
       <Route path="/message/:type" element={<MessagePage />} />
 
-      {/* 404 */}
       <Route path="*" element={<div className="p-4">Not Found</div>} />
     </Routes>
   );
